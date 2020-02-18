cadbeb8e8c7194a3cb3ccf52699b2c414f3a49b6
Brandon Bernal <bernal.brandon@gmail.com>
2020-02-18 02:01:40 -0500

    Tied card flipping logic to click events.

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -32,13 +32,24 @@ function checkForMatch() {
     }
 };
 
-function flipCard(cardId) {
+function flipCard() {
+    const cardId = this.getAttribute('data-id');
     cardsInPlay.push(cards[cardId].rank);
     console.log("User flipped "+ cards[cardId].rank + " of " + cards[cardId].suit + ": " + cards[cardId].cardImage);
+    this.setAttribute('src', cards[cardId].cardImage);
     if (cardsInPlay.length === 2) {
         checkForMatch();
     }
 };
 
-flipCard(0);
-flipCard(2);
+function createBoard() {
+    for (let i = 0; i < cards.length; i++) {
+        const cardElement = document.createElement('img');
+        cardElement.setAttribute('src', 'images/back.png');
+        cardElement.setAttribute('data-id', i);
+        cardElement.addEventListener('click', flipCard);
+        document.getElementById('game-board').appendChild(cardElement);
+    }
+}
+
+createBoard();
